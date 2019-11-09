80acb5965b8fe0c0e9ebebe132c83cfabd74e9d8
J0nrad <radstackproduction@Radstacks-iMac.cable.rcn.com>
2019-11-09 14:21:19 -0500

    actions, reducers, util, store

--- frontend/onboard.jsx
+++ frontend/onboard.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { id: window.currentUser.id },
+      entities: {
+        users: { [ window.currentUser.id ]: window.currentUser }
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById('root');
   ReactDOM.render(<App/>, root);
 });
---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,22 @@
+import {
+   RECEIVE_SESSION_ERRORS,
+   RECEIVE_CURRENT_USER,
+   REMOVE_ERRORS
+} from '../actions/session_actions';
+import {
+   RECEIVE_ERRORS,
+   REMOVE_ERRORS
+} from '../actions/error_actions';
+
+export default (state = [], action) => {
+   switch (action.type) {
+      case RECEIVE_CURRENT_USER:
+         return [];
+      case RECEIVE_ERRORS:
+         return action.errors;
+      case REMOVE_ERRORS:
+         return [];
+      default:
+         return state;
+   }
+};
