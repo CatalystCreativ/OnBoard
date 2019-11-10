df5e5f5bb4f9c8133c776950d044a6286b7a367e
J0nrad <radstackproduction@Radstacks-iMac.cable.rcn.com>
2019-11-10 15:56:05 -0500

    product containers

--- frontend/components/products/product_item.jsx
+++ frontend/components/products/product_item.jsx
@@ -4,7 +4,14 @@ import { Link } from 'react-router-dom';
 class ProductItem extends React.Component {
    constructor(props) {
       super(props);
-      this.product = this.props.product
+      this.product = this.props.product;
+      this.addFavorite = this.props.addFavorite;
+      this.handleFavorite = this.handleFavorite.bind(this);
+   }
+
+   handleFavorite() {
+      const productId = this.product.id;
+      this.addFavorite(productId)
    }
 
    render() {
@@ -12,7 +19,7 @@ class ProductItem extends React.Component {
 
       return (
          <Link to={`/products/${product.id}`}>
-            <button>Favorite</button>
+            <button onClick={this.handleFavorite}>Favorite</button>
             <button>Message</button>
             <div className="project_image"></div>
             <label>{product.title}</label>
