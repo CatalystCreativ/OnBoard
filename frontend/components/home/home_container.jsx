ad9d0f5a95e8548810af9183870b712539ce0207
J0nrad <radstackproduction@Radstacks-iMac.cable.rcn.com>
2019-11-10 16:41:54 -0500

    home component

--- frontend/components/home/home_container.jsx
+++ frontend/components/home/home_container.jsx
@@ -3,9 +3,26 @@ import { receiveProducts } from '../../actions/product_actions';
 import Home from './home';
 
 const msp = (state) => {
-   const products = state.entities.products;
+   const products = {"surf": [], "skate": [], "snow": []};
+   const currentUser = state.session.currentUser;
+
+   if ( state.entities.products ) {
+      for (let i = 0; i < state.entities.products.length; i++) {
+         const product = state.entities.products[i];
+
+         if (product.category === "surf") {
+            products["surf"].push(product);
+         } else if (product.category === "skate") {
+            products["skate"].push(product);
+         } else if (product.category === "snow") {
+            products["snow"].push(product);
+         }
+      }
+   }
+
    return {
-      products
+      products,
+      currentUser
    }
 }
 
