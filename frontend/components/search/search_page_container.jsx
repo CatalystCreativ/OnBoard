7201056b06f4d4dbfc7ac026f0283db20e94cd06
dchan440 <dchan440@bloomberg.net>
2019-11-19 21:35:50 -0500

    remove categories from products parsing

--- frontend/components/search/search_page_container.jsx
+++ frontend/components/search/search_page_container.jsx
@@ -3,26 +3,10 @@ import { receiveProducts } from '../../actions/product_actions';
 import Home from './home';
 
 const msp = (state) => {
-    const products = { "surf": [], "skate": [], "snow": [] };
-    const currentUser = state.session.currentUser;
+    const products = state.entities.products;
     const searchFilters = state.entities.searchFilters
-    if (state.entities.products) {
-        for (let i = 0; i < state.entities.products.length; i++) {
-            const product = state.entities.products[i];
-
-            if (product.category === "surf") {
-                products["surf"].push(product);
-            } else if (product.category === "skate") {
-                products["skate"].push(product);
-            } else if (product.category === "snow") {
-                products["snow"].push(product);
-            }
-        }
-    }
-
     return {
         products,
-        currentUser,
         searchFilters
     }
 }
